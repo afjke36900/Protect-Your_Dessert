478dd863b1ef377249e051aad6dc1aad9ef3f642
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-06-02 16:30:30 +0800

    螢幕解析度

---
+++ Protect_Your_Dessert/Assets/Scripts/GameControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public KeyCode forward = KeyCode.W;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(forward))
+        {
+            print("前進");
+        }
+    }
+
+    public void ChangeKey(KeyCode key)
+    {
+        forward = key;
+    }
+}
