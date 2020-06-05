238eb6e1d900c8f9707e5cd880fb39fd4e1cde17
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-06-05 16:32:58 +0800

    更新UI

--- Protect_Your_Dessert/Assets/Scripts/GameControl.cs
+++ Protect_Your_Dessert/Assets/Scripts/GameControl.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class GameControl : MonoBehaviour
 {
+    public KeyCode jump = KeyCode.Space;
     public KeyCode forward = KeyCode.W;
+    public KeyCode left = KeyCode.A;
+    public KeyCode backward = KeyCode.S;
+    public KeyCode right = KeyCode.D;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +19,34 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(jump))
+        {
+            print("跳躍");
+        }
         if (Input.GetKeyDown(forward))
         {
             print("前進");
+        }
+        if (Input.GetKeyDown(left))
+        {
+            print("向左");
+        }
+        if (Input.GetKeyDown(backward))
+        {
+            print("後退");
+        }
+        if (Input.GetKeyDown(right))
+        {
+            print("向右");
         }
     }
 
     public void ChangeKey(KeyCode key)
     {
+        jump = key;
         forward = key;
+        left = key;
+        backward = key;
+        right = key;
     }
 }
--- Protect_Your_Dessert/Assets/Scripts/GameSet.cs
+++ Protect_Your_Dessert/Assets/Scripts/GameSet.cs
@@ -8,50 +8,40 @@ public class GameSet : MonoBehaviour
     [Header("是否全螢幕")]
     public Toggle toggle;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+        
     void Update()
     {
         #region 螢幕解析度 if 寫法
-        if (toggle == false)
-        {
-            if (ScreenSizeDropdown.value == 0)
-            {
-                Screen.SetResolution(1920, 1080, false);
-            }
+        //if (toggle == false)
+        //{
+                    if (ScreenSizeDropdown.value == 0)
+                    {
+                        Screen.SetResolution(1920, 1080, false);
+                    }
 
-            if (ScreenSizeDropdown.value == 1)
-            {
-                Screen.SetResolution(1280, 720, false);
-            }
+                    if (ScreenSizeDropdown.value == 1)
+                    {
+                        Screen.SetResolution(1280, 720, false);
+                    }
 
-            if (ScreenSizeDropdown.value == 2)
-            {
-                Screen.SetResolution(960, 540, false);
-            }
+                    if (ScreenSizeDropdown.value == 2)
+                    {
+                        Screen.SetResolution(960, 540, false);
+                    }
+
+                    if (ScreenSizeDropdown.value == 3)
+                    {
+                        Screen.SetResolution(1920, 1080, true);
         }
-        else
-        {
-            if (ScreenSizeDropdown.value == 0)
-            {
-                Screen.SetResolution(1920, 1080, true);
-            }
-
-            if (ScreenSizeDropdown.value == 1)
-            {
-                Screen.SetResolution(1280, 720, true);
-            }
-
-            if (ScreenSizeDropdown.value == 2)
-            {
-                Screen.SetResolution(960, 540, true);
-            }
-        }
+        //}
+        //else
+        //{
+            
+        //}
         #endregion
 
         #region 螢幕解析度 Switch 寫法
