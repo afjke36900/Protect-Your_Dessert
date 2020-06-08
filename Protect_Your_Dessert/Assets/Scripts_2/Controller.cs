863a086462754514f592479134a59f2920b5c8bd
afjke36900 <afjke36900@gmail.com>
2020-06-09 05:58:16 +0800

    更新UI

---
+++ Protect_Your_Dessert/Assets/Scripts_2/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(GameManager.GM.forward))
+            transform.position += Vector3.forward / 20;
+        if (Input.GetKey(GameManager.GM.backward))
+            transform.position += -Vector3.forward / 20;
+        if (Input.GetKey(GameManager.GM.left))
+            transform.position += Vector3.left / 20;
+        if (Input.GetKey(GameManager.GM.right))
+            transform.position += Vector3.right / 20;
+        if (Input.GetKey(GameManager.GM.jump))
+            transform.position += Vector3.up / 20;
+    }
+}
