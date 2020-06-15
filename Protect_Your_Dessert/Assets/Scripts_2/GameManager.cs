6a7c9d20a89a3507f4bc09baf501cf7edfae5a0d
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-06-15 08:36:56 +0800

    0615 merge

---
+++ Protect_Your_Dessert/Assets/Scripts_2/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GM;
+
+    public KeyCode jump { set; get; }
+    public KeyCode forward { set; get; }
+    public KeyCode left { set; get; }
+    public KeyCode backward { set; get; }
+    public KeyCode right { set; get; }
+    
+    private void Awake()
+    {
+        if (GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
+        else if(GM !=this)
+        {
+            Destroy(gameObject);
+        }
+
+        jump = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("jumpKey", "Space"));
+        forward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("forwardpKey", "W"));
+        left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("leftKey", "A"));
+        backward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("backwardKey", "S"));
+        right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("rightKey", "D"));
+    }
+}
