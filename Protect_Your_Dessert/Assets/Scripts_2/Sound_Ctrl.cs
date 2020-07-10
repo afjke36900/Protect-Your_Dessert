929d73c0c9727837048ba714b8f1d4d5d39511c7
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-07-10 10:33:22 +0800

    微調程式

---
+++ Protect_Your_Dessert/Assets/Scripts_2/Sound_Ctrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Ctrl : MonoBehaviour
+{
+    bool ControlSound;
+
+    public void SoundButton()
+    {
+        ControlSound = !ControlSound;
+        AudioListener.pause = ControlSound;
+    }
+}
+
